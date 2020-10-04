baae5605dee3079027472dfc451bb750b6272145
blox-dev <bloxorz51gaming@gmail.com>
2020-10-04 20:52:59 +0300

    Added dance object

---
+++ Vuforia/Assets/stopAnimation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stopAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Animator anim;
+    public Camera ar_camera;
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        anim.speed = 0;
+    }
+
+    void Update()
+    {
+        Transform obj_transform = GetComponent<Transform>();
+        Debug.Log(Vector3.Distance(obj_transform.position, ar_camera.transform.position));
+        if (Vector3.Distance(obj_transform.position, ar_camera.transform.position) < 900.0f)
+        {
+            anim.speed = 1;
+        }
+        else
+        {
+            anim.speed = 0;
+        }
+    }
+}
